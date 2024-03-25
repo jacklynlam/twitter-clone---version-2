33bec554ea5a877b899df51dfccf967b85a915e2
Jacklyn <lamjacklyn@gmail.com>
2024-03-25 09:56:58 +0800

    add firebase configuration

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,33 @@
+import { getAuth } from 'firebase/auth';
+import { useEffect, useContext } from 'react';
+import { Navbar, Container, Button, Row } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../components/AuthProvider';
+import useLocalStorage from 'use-local-storage';
+import ProfileSideBar from '../components/ProfileSideBar';
+import ProfileMidBody from '../components/ProfileMidBody';
+
+export default function ProfilePage() {
+    const auth = getAuth();
+    const navigate = useNavigate();
+    const { currentUser } = useContext(AuthContext);
+
+    if (!currentUser) {
+        navigate('/login');
+    }
+
+    const handleLogout = () => {
+        auth.signOut();
+    };
+
+    return (
+        <>
+            <Container>
+                <Row>
+                    <ProfileSideBar handleLogout={handleLogout} />
+                    <ProfileMidBody />
+                </Row>
+            </Container>
+        </>
+    );
+}
